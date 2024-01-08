cfb93aa4895b08496353e0b5150080dee4c8d9d9
Thomas Wutzler <twutz@bgc-jena.mpg.de>
2024-01-08 15:35:50 +0100

    test sim_sols

--- test/test_util_mixed.jl
+++ test/test_util_mixed.jl
@@ -6,55 +6,51 @@ using MTKHelpers
 using DataFrames
 using ComponentArrays: ComponentArrays as CA
 
-# df_site_u0_p = DataFrames.DataFrame([
-#         (:siteA,
-#             CA.ComponentVector(mv₊x = [2.1,2.2]),
-#             CA.ComponentVector(mv₊p1 = 10, mv₊p2 = 5.2, mv₊τ = 0.1)),
-#         (:siteB,
-#             CA.ComponentVector(mv₊x = 1.8),
-#             CA.ComponentVector(mv₊p1 = 11, mv₊p2 = 4.8, mv₊τ = 0.1)),
-#     ], [:site, :u0, :p])
-
-# u0 = df_site_u0_p.u0[1]
-# p = df_site_u0_p.p[1]
-@named mv = CP.samplesystem_vec()
-@named system = embed_system(mv)
+@named sv = CP.samplesystem_vec()
+@named system = embed_system(sv)
 scenario = (system = :CrossInverts_samplesystem_vec,)
 
-popt = CA.ComponentVector(mv₊x = [3.1, 3.2], mv₊τ = 0.5, mv₊p = 1:3)
+fixed = CA.ComponentVector(sv₊p = 1:3)
+random = CA.ComponentVector(sv₊x = [2.1,2.2], sv₊τ = 0.1)
+indiv = CA.ComponentVector(sv₊i = 0.2)
+popt = vcat_statesfirst(fixed, random, indiv; system)
 
-u0 = CA.ComponentVector(mv₊x = [3.11, 3.21])
-p = CA.ComponentVector(mv₊τ = 0.51)
+# fixed = CA.ComponentVector(par=(sv₊p = 1:3,))
+# random = CA.ComponentVector(state=(sv₊x = [2.1,2.2],), par=(sv₊τ = 0.1,))
+# indiv = CA.ComponentVector(par=(sv₊i = 0.2,))
+# popt = merge_subvectors(fixed, random, indiv; mkeys=(:state, :par))
 
-tools = setup_tools_scenario(:A, scenario, popt; system);
-probo = remake(tools.problem, popt, tools.pset)
+toolsA = setup_tools_scenario(:A, scenario, popt; system);
 
-fixed = CA.ComponentVector(mv₊p = 1:3)
-random = CA.ComponentVector(mv₊x = [2.1,2.2], mv₊τ = 0.1)
-keys_indiv = (:mv₊i,)
-keys_popt = (keys(fixed)..., keys(random)..., keys_indiv...)
+psets = setup_psets_fixed_random_indiv(system, popt, keys(fixed), keys(random))
 
-psets = setup_psets_fixed_random_indiv(get_system(tools.problem), keys_popt,
-    keys(fixed), keys(random))
-
+@testset "setup_psets_fixed_random_indiv" begin
+    @test all((:fixed, :random, :indiv) .∈ Ref(keys(psets)))
+    @test psets.fixed isa ODEProblemParSetter 
+    @test get_paropt_labeled(toolsA.pset, toolsA.problem; flatten1=Val(true)) == popt
+    @test get_paropt_labeled(psets.fixed, toolsA.problem; flatten1=Val(true)) == fixed
+    @test get_paropt_labeled(psets.random, toolsA.problem; flatten1=Val(true)) == random
+    @test get_paropt_labeled(psets.indiv, toolsA.problem; flatten1=Val(true)) == indiv
+end;
 
 df_site_u0_p = DataFrame(
     site = [:A, :B, :C],
-    u0 = fill((label_state(tools.pset, tools.problem.u0)), 3),  
-    p = fill((label_par(tools.pset, tools.problem.p)), 3),   
+    u0 = fill((label_state(toolsA.pset, toolsA.problem.u0)), 3),  
+    p = fill((label_par(toolsA.pset, toolsA.problem.p)), 3),   
 )
 df = copy(df_site_u0_p)
-# compute indiv_random by indiv/random to columns :indiv_random
-_compute_indiv_rand = (u0, p) -> begin
-    local popt = get_paropt_labeled(tools.pset, u0, p; flatten1=Val(true))
-    # k = first(keys(random))
-    gen = (popt[k] ./ random[k] for k in keys(random))
-    v = reduce(vcat, gen)
-    MTKHelpers.attach_axis(v, first(CA.getaxes(random)))
+
+
+@testset "gen_compute_indiv_rand" begin
+    _compute_indiv_rand = gen_compute_indiv_rand(toolsA.pset, random)    
+    tmp = _compute_indiv_rand(toolsA.problem.u0, toolsA.problem.p)
+    @test keys(tmp) == keys(random)
+    tmp[:sv₊x] = popt[:sv₊x] ./ random[:sv₊x] # popt -> toolsA -> df_site.u0/p
 end
-tmp = _compute_indiv_rand(df.u0[1], df.p[1])
+
+_compute_indiv_rand = gen_compute_indiv_rand(toolsA.pset, random)    
 DataFrames.transform!(df,
-    [:u0, :p] => DataFrames.ByRow(_compute_indiv_rand) => :indiv_random)
+[:u0, :p] => DataFrames.ByRow(_compute_indiv_rand) => :indiv_random)
 
 # extract the parameters to optimize that are individual-specific to clumns :indiv
 _extract_indiv = (u0, p) -> vcat(u0, p)[symbols_paropt(psets.indiv)]
@@ -65,17 +61,24 @@ _setuptools = (u0, p) -> setup_tools_scenario(:A, scenario, popt; system, u0, p)
 _setuptools(df.u0[1], df.p[1]);
 DataFrames.transform!(df, [:u0, :p] => DataFrames.ByRow(_setuptools) => :tool)
 
-sim_sols_probs = gen_sim_sols_probs(; tools = df.tool, psets)
-sols_probs = sim_sols_probs(fixed, random, hcat(df.indiv...), hcat(df.indiv_random...));
-sol = first(sols_probs).sol;
-sol.t
-sol[mv.x]
+@testset "simsols" begin
+    solver = AutoTsit5(Rodas5P())
+    sim_sols_probs = gen_sim_sols_probs(; tools = df.tool, psets, solver)
+    sols_probs = sim_sols_probs(fixed, random, hcat(df.indiv...), hcat(df.indiv_random...));
+    sol = first(sols_probs).sol;
+    solA0 = solve(toolsA.problem, solver) # same as with initial popt -> recomputed
+    solA0 == sol
+    #sol.t
+    #sol[sv.x]
 
-sim_sols = gen_sim_sols(; tools = df.tool, psets, maxiters = 1e4)
-poptl = (;
-    fixed,
-    random,
-    indiv = hcat(df.indiv...),
-    indiv_random = hcat(df.indiv_random...),
-    )
-sols = sim_sols(poptl);
+    sim_sols = gen_sim_sols(; tools = df.tool, psets, solver, maxiters = 1e4)
+    poptl = (;
+        fixed,
+        random,
+        indiv = hcat(df.indiv...),
+        indiv_random = hcat(df.indiv_random...),
+        )
+    sols = sim_sols(poptl);
+    sol = first(sols);
+    solA0 == sol
+end;
